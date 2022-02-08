ce1af0c87ce16551f2212bbb59be34a4a0178294
lukemjohnston <72853815+lukemjohnston@users.noreply.github.com>
2022-02-08 14:37:34 -0800

    skeleton for sign_up and sign_in

--- app/src/main/java/com/example/dvt_app/SignIn_Activity.java
+++ app/src/main/java/com/example/dvt_app/SignIn_Activity.java
@@ -2,13 +2,54 @@ package com.example.dvt_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import org.w3c.dom.Text;
+
+
 
 public class SignIn_Activity extends AppCompatActivity {
+
+    private DatabaseReference root = FirebaseDatabase.getInstance().getReference().getRoot();
+
+    private DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        Button button = (Button)findViewById(R.id.SignIn);
+        TextView signUp = (TextView) findViewById(R.id.TextViewSignUp);
+        TextView email = (TextView) findViewById(R.id.EditTextEmail);
+        TextView password = (TextView) findViewById(R.id.EditTextPassword);
+
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignIn_Activity.this, SignUp_Activity.class);
+                startActivity(intent);
+
+            }
+        });
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+            }
+        });
     }
+
+
+
 }
