0d2717e919a70ff27b80cc74519b028940bbf060
lukemjohnston <72853815+lukemjohnston@users.noreply.github.com>
2022-02-18 01:41:37 -0800

    Sign in and Sign up changes
    not working on my machine but it should. idk what's wrong

--- app/src/main/java/com/example/dvt_app/SignUp_Activity.java
+++ app/src/main/java/com/example/dvt_app/SignUp_Activity.java
@@ -16,6 +16,10 @@ import android.widget.Toast;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.firestore.IgnoreExtraProperties;
@@ -23,52 +27,59 @@ import com.google.firebase.firestore.auth.User;
 
 public class SignUp_Activity extends AppCompatActivity {
 
+    private final FirebaseAuth mAuth = FirebaseAuth.getInstance();
     FirebaseDatabase database = FirebaseDatabase.getInstance();
     DatabaseReference mDatabase = database.getReference();
 
+
     @IgnoreExtraProperties
-    public class User {
+    public static class User {
 
         public String Name;
-        public String Email;
         public String Phone;
         public String Birthdate;
-        public String Password;
 
         public User() {
             // Default constructor required for calls to DataSnapshot.getValue(User.class)
         }
 
-        public User(String name, String email, String phone, String birthdate, String password) {
+        public User(String name, String phone, String birthdate) {      //fills user variable with the user data
             this.Name = name;
-            this.Email = email;
             this.Phone = phone;
             this.Birthdate = birthdate;
-            this.Password = password;
         }
-
     }
 
 
+    public void writeNewUser(String name, String email, String phone, String birthdate, String password) {
 
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {     //creates a authenticated user account
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {      // Sign up success
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser userAuth = mAuth.getCurrentUser();     //set user to the newly created user
 
-    public void writeNewUser(String username, String name, String email, String phone, String birthdate, String password) {
-        User user = new User(name, email, phone, birthdate, password);
-        mDatabase.child("users").child(username).setValue(user)
-                .addOnSuccessListener(new OnSuccessListener<Void>() {
-            @Override
-            public void onSuccess(Void aVoid) {
-                Toast toast = Toast.makeText(getApplicationContext(), "ACCOUNT CREATED", Toast.LENGTH_LONG);
-                toast.show();
-            }
-        }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                Toast toast = Toast.makeText(getApplicationContext(), "ERROR", Toast.LENGTH_LONG);
-                toast.show();
-            }
-        });
+                            User user = new User(name, phone, birthdate);
+                            assert userAuth != null;
+                            mDatabase.child("users").child(userAuth.getUid()).setValue(user)   //store other user data, with the key being the auth user ID
+                                    .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                        @Override
+                                        public void onSuccess(Void aVoid) {
+                                            Toast.makeText(getApplicationContext(), "ACCOUNT CREATED", Toast.LENGTH_LONG).show();
+                                        }
+                                    });
+
+                        } else {    //sign up fails
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(getApplicationContext(), "ACCOUNT CREATION FAILED",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -76,31 +87,22 @@ public class SignUp_Activity extends AppCompatActivity {
         setContentView(R.layout.activity_sign_up);
 
         Button submit = (Button)findViewById(R.id.SignUpButton);
-        TextView usernameTV = (TextView) findViewById(R.id.editTextUsername);
-        TextView nameTV = (TextView) findViewById(R.id.editTextName);
-        TextView emailTV = (TextView) findViewById(R.id.editTextEmail);
-        TextView phoneTV = (TextView) findViewById(R.id.editTextPhone);
-        TextView birthdateTV = (TextView) findViewById(R.id.editTextDate);
-        TextView passwordTV = (TextView) findViewById(R.id.editTextPassword);
 
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String Username = usernameTV.getText().toString();
-                String Name = nameTV.getText().toString();
-                String Email = emailTV.getText().toString();
-                String Phone = phoneTV.getText().toString();
-                String Birthdate = birthdateTV.getText().toString();
-                String Password = passwordTV.getText().toString();
+                String Name = findViewById(R.id.editTextName).toString();
+                String Email = findViewById(R.id.editTextEmail).toString();
+                String Phone = findViewById(R.id.editTextPhone).toString();
+                String Birthdate = findViewById(R.id.editTextDate).toString();
+                String Password = findViewById(R.id.editTextPassword).toString();
 
-                writeNewUser(Username, Name, Email, Phone, Birthdate, Password);
+                writeNewUser(Name, Email, Phone, Birthdate, Password);
 
-                Intent intent = new Intent(SignUp_Activity.this, SignIn_Activity.class);
-                startActivity(intent);
-
+                //Intent intent = new Intent(SignUp_Activity.this, SignIn_Activity.class);
+                //startActivity(intent);
             }
         });
     }
-
 
 }
